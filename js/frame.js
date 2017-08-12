0a8e78e845c2e0bbfef3dc3e906c464e39ffe95e
15ay3 <andus401@gmail.com>
2017-08-12 18:38:41 -0400

    frame

--- js/frame.js
+++ js/frame.js
@@ -3,6 +3,11 @@
       var remote = require('electron').remote;
 
      function init() {
+        document.getElementById("min-btn").addEventListener("click", function (e) {
+              var window = remote.getCurrentWindow()
+              window.minimize();
+         });
+
           document.getElementById("close-btn").addEventListener("click", function (e) {
                var window = remote.getCurrentWindow()
                window.close();
@@ -16,3 +21,11 @@
      };
 
 })();
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
