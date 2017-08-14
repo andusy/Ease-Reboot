62cf5b41a36e8a3ee68d141a0f0078460466ca08
15ay3 <andus401@gmail.com>
2017-08-14 03:55:31 -0400

    modals

--- js/frame.js
+++ js/frame.js
@@ -8,6 +8,49 @@
               window.minimize();
          });
 
+         ///////////////////////////////////////////////////
+         /////////   Help Modal  //////////////////////////
+         /////////////////////////////////////////////////
+         var helpModal = document.getElementById('help-modal');
+         var helpBTN = document.getElementById("help-btn");
+         var helpSP = document.getElementById('help-close');
+
+         // When the user clicks on the button, open the modal
+         document.getElementById("help-btn").addEventListener("click", function (e) {
+            helpModal.style.display = "block";
+         });
+
+         // When the user clicks on <span> (x), close the modal
+         helpSP.onclick = function() {
+             helpModal.style.display = "none";
+         }
+
+         ///////////////////////////////////////////////////
+         /////////   Settings Modal  //////////////////////
+         /////////////////////////////////////////////////
+         var setModal = document.getElementById('settings-modal');
+         var setBTN = document.getElementById("settings-btn");
+         var setSP = document.getElementById('settings-close');
+
+         // When the user clicks on the button, open the modal
+         document.getElementById("settings-btn").addEventListener("click", function (e) {
+            setModal.style.display = "block";
+         });
+
+         // When the user clicks on <span> (x), close the modal
+         setSP.onclick = function() {
+             setModal.style.display = "none";
+         }
+
+         // When the user clicks anywhere outside of the modal, close it
+         window.onclick = function(event) {
+             if (event.target == setModal) {
+                 setModal.style.display = "none";
+             } else if (event.target == helpModal){
+                helpModal.style.display = "none";
+             }
+         }
+
           document.getElementById("close-btn").addEventListener("click", function (e) {
                var window = remote.getCurrentWindow()
                window.close();
--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow(){
   }));
 
   // Open Devtools
-  //win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
   win.on('closed', () =>{
     win = null;
