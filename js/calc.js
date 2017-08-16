3b1b0dbf551caa5fe9c8fa3a413148de7e7f2b19
15ay3 <andus401@gmail.com>
2017-08-16 03:22:15 -0400

    add buttons

---
+++ js/calc.js
@@ -0,0 +1,17 @@
+function addTax(){
+  var list = document.getElementById('tax-list');
+  var li = document.createElement('li');
+  li.innerHTML = 'placeholder';
+  list.appendChild(li);
+}
+
+function addNonTax(){
+  var list = document.getElementById('untax-list');
+  var li = document.createElement('li');
+  li.innerHTML = 'placeholder';
+  list.appendChild(li);
+}
+
+function addAmount(){
+  
+}
--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow(){
   }));
 
   // Open Devtools
-  //in.webContents.openDevTools();
+  //win.webContents.openDevTools();
 
   win.on('closed', () =>{
     win = null;
